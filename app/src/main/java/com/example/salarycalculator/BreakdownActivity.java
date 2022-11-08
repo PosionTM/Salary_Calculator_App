ecbd3cc9ad1bb7e59590815788f4478f1aab7b0b
PosionTM <morillo.tommy@gmail.com>
2022-11-08 17:30:53 -0500

    Adding breakdown activity for future update

---
+++ app/src/main/java/com/example/salarycalculator/BreakdownActivity.java
@@ -0,0 +1,14 @@
+package com.example.salarycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BreakdownActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breakdown);
+    }
+}
